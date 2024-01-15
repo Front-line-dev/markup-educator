8b4c0fbe16fb5e80bf81c80059fdce437dff9a78
jin-young-lee <jin-young-lee@nts-corp.com>
2024-01-16 00:24:54 +0900

    CSS 에러 추가

--- src/component/Canvas.tsx
+++ src/component/Canvas.tsx
@@ -10,23 +10,28 @@ interface CanvasProps {
 
 export default function Canvas({ html, css, type }: CanvasProps) {
   const [compiledCss, setCompiledCss] = useState('');
+  const [isShowCssError, setIsShowCssError] = useState(false);
 
   useEffect(() => {
     Sass.compile(css, (result) => {
       if (result.status === 0) {
         setCompiledCss(result.text);
+        setIsShowCssError(false);
       } else {
-        console.error(result);
+        setIsShowCssError(true);
       }
     });
   }, [css]);
 
   return (
-    <iframe
-      srcDoc={`<style>${compiledCss}</style>${html}<script>window.addEventListener('load', () => {window.top.postMessage('', '*');})</script>`}
-      title="Rendered codes"
-      className={styles.canvas}
-      data-type={type}
-    />
+    <div className={styles.wrap}>
+      <iframe
+        srcDoc={`<style>${compiledCss}</style>${html}<script>window.addEventListener('load', () => {window.top.postMessage('', '*');})</script>`}
+        title="Rendered codes"
+        className={styles.canvas}
+        data-type={type}
+      />
+      {isShowCssError && <p className={styles.error}>Error : CSS 문법이 잘못되었습니다.</p>}
+    </div>
   );
 }
